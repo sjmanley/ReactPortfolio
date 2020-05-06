88e48af619605269a498611d7f62788f45f8069b
Scott Manley <scottmanley@SJM-MacBook-Pro.local>
2020-05-05 19:32:51 -0500

    working on portfolio pages

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -27,7 +27,7 @@ function NavTabs(props) {
           onClick={() => props.handlePageChange("Blog")}
           className={props.currentPage === "Blog" ? "nav-link active" : "nav-link"}
         >
-          Blog
+          Portfolio
         </a>
       </li>
       <li className="nav-item">
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,13 +2,23 @@ import React from "react";
 
 const About = () => (
   <div>
-    <h1>About Page</h1>
+    <h1>About Me</h1>
     <p>
-      Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui mauris,
-      ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus porta. Nam
-      quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-      imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed rhoncus mollis
-      diam, sit amet facilisis lectus blandit at.
+      
+    SJM Fine Design is a professional design firm. Full-Stack developing with skills in HTML, CSS, JavaScript,
+          Bootstrap,
+          JQuery, Node, MySQL, Databases and MongoDB. Skills to individualize your fine art needs and enhance your
+          personal life style. We work closely with our clients to ensure a total custom experience and create a
+          final product that will enrich any project.I have a passion for code development as well as art and
+          design with extensive experience in
+          graphic design. I have gained strong analytical skills with the ability to develop graphics with multiple
+          applications. <br></br>
+          Adept working within a team or individually, problem solving projects in a collaborative environment.
+          I like the idea of taking an idea, whether it’s in a group or solo, from concept to finished product.
+          Delivering it to the client exceeding their expectations.
+          Art has always been very exciting to me. Creating on a canvas or on a computer. For print or for the
+          internet.
+          Being creative and executing a top quality product is my foremost goal.
     </p>
   </div>
 );
